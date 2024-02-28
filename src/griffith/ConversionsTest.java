d181dec3c6b420bf0366d9ed9f0f7f4857cf91a4
sofia.kilipko <sofia.kilipko@gmail.com>
2024-02-28 19:52:51 +0000

    testCelsiusToFahrenheit() test implimentation

--- src/griffith/ConversionsTest.java
+++ src/griffith/ConversionsTest.java
@@ -8,7 +8,14 @@ class ConversionsTest {
 
 	@Test
 	void testCelsiusToFahrenheit() {
-		fail("Not yet implemented");
+		//creation of new conversion object to test it's celsius to fahrenheit convertion
+		Conversions converter = new Conversions();
+		// after convertion, 0 degree in celsius will give 32 degree in fahrenheit
+		assertEquals(converter.celsiusToFahrenheit(0),32);
+		// after convertion, -10 degree in celsius will give 14 degree in fahrenheit
+		assertEquals(converter.celsiusToFahrenheit(-10),14);
+		// after convertion, 36.6 degree in celsius will give 97.88 degree in fahrenheit
+		assertEquals(converter.celsiusToFahrenheit(36.6),97.88);
 	}
 
 	@Test
