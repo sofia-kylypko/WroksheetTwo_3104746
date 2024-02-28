19adef6fa2fcb27e315c2ecd035d3f4edd547c60
sofia.kilipko <sofia.kilipko@gmail.com>
2024-02-28 12:42:22 +0000

    stubs methods in Conversions.java added

--- src/griffith/Conversions.java
+++ src/griffith/Conversions.java
@@ -4,7 +4,37 @@ public class Conversions {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 	}
-
+	
+	//method to convert given temperature in celsius to Fahrenheit
+	public double celsiusToFahrenheit(double tempCelsius) {
+		return tempCelsius;
+	}
+	
+	//method to convert given temperature in Fahrenheit to celsius
+	public double fahrenheitToCelsius(double tempFahrenheit) {
+		return tempFahrenheit;
+	}
+	
+	//method to convert given distance from miles to kilometer 
+	public double milesToKilometer(double distanceMiles) {
+		return distanceMiles;
+	}
+	
+	//method to convert given distance from kilometer to miles
+	public double kilometerToMiles(double distanceKilometers) {
+		return distanceKilometers;
+	}
+	
+	//method to switch all upper case values to lower case values
+	public String upperCaseToLowerCase() {
+		return null;
+	}
+	
+	//method to switch all lower case values to upper case values
+	public String lowerCaseToUpperCase() {
+		return null;
+	}
+	
 }
+
